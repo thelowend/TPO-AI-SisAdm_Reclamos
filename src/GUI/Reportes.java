5114074f5825e3108a2cfad7ffcf32c8e974c241
thelowend <thelowend6@gmail.com>
2016-11-04 11:59:27 -0300

    Segundo Reporte

--- src/GUI/Reportes.java
+++ src/GUI/Reportes.java
@@ -8,6 +8,8 @@ import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 import Main.Controller;
+import Model.EstadoReclamo;
+import Vistas.DetalleReclamoView;
 import Vistas.ReclamoView;
 
 import javax.swing.JComboBox;
@@ -16,7 +18,12 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.print.PrinterException;
+import java.time.Year;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Map.Entry;
 import java.awt.event.ActionEvent;
@@ -27,6 +34,7 @@ public class Reportes extends JFrame {
 
 	private JPanel contentPane;
 	private JTable tblReporte;
+	private ArrayList<ReclamoView> reclamosView;
 
 	/**
 	 * Launch the application.
@@ -67,47 +75,89 @@ public class Reportes extends JFrame {
 		contentPane.add(lblTipoDeReporte);
 		
 		JButton btnGenerar = new JButton("Generar");
+		
 		btnGenerar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 			    switch (cmbReportes.getSelectedIndex()) {
             	case 0: 
-            		System.out.println("0");
-            		ArrayList<ReclamoView> reclamosView = Controller.getInstancia().listReclamoView();
-            		Hashtable<Integer, Integer> table = new Hashtable<Integer, Integer>();
-            		
-            		String[] headers = {"ID Cliente", "Cantidad de Reclamos"};
-            		
+            		//ranking de clientes con mayor reclamos
+            		reclamosView = Controller.getInstancia().listReclamoView();
+            		Hashtable<Integer, Integer> htMasReclamos = new Hashtable<Integer, Integer>();
+
             		for (int i = 0; i < reclamosView.size(); i++) {
             			int clienteId = reclamosView.get(i).getClienteId();
-            			if (!table.containsKey(clienteId)) {
-            				table.put(clienteId, 1);
+            			if (!htMasReclamos.containsKey(clienteId)) {
+            				htMasReclamos.put(clienteId, 1);
             			} else {
-            				table.put(clienteId,  table.get(clienteId) + 1);
+            				htMasReclamos.put(clienteId,  htMasReclamos.get(clienteId) + 1);
             			} 			
             		}
             		
-            	    DefaultTableModel tblReporteModel = new DefaultTableModel(headers, 0);
-            	    tblReporteModel.setColumnIdentifiers(headers);
+            		String[] headersMasReclamos = {"ID Cliente", "Cantidad de Reclamos"};
+            	    DefaultTableModel tblReporteModelMasReclamos = new DefaultTableModel(headersMasReclamos, 0);
             	    
             		
-            		for(Entry<?, ?> entry: table.entrySet()) {
-            			tblReporteModel.addRow(new Object[] {entry.getKey(), entry.getValue()});
+            		for(Entry<?, ?> entry: htMasReclamos.entrySet()) {
+            			tblReporteModelMasReclamos.addRow(new Object[] {entry.getKey(), entry.getValue()});
             		}
             		
-            		tblReporte.setModel(tblReporteModel);
+            		tblReporte.setModel(tblReporteModelMasReclamos);
             		tblReporte.setAutoCreateRowSorter(true);
+            		
             		//Ordena por mayor cantidad de reclamos
             		tblReporte.getRowSorter().toggleSortOrder(1);
             		
 	        		break;
             	case 1: 
-        			System.out.println("1");
+            		//cantidad de reclamos tratados por mes
+            		reclamosView = Controller.getInstancia().listReclamoView();
+            		Calendar cal = Calendar.getInstance();
+            		String[] mes = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+            		
+            		
+            		ArrayList<ReclamoView> reclamosView = Controller.getInstancia().listReclamoView();
+            		Hashtable<String, Integer> htReclamosMensales = new Hashtable<String, Integer>();
+
+            		for (int i = 0; i < reclamosView.size(); i++) {
+            			HashMap<EstadoReclamo, DetalleReclamoView> hashReclamo = reclamosView.get(i).getHashReclamos();
+            			
+            	        for (EstadoReclamo key : hashReclamo.keySet()) {
+            	        	Date fechaCierre = hashReclamo.get(key).getFechaCierre();
+            	        	
+            	        	if(fechaCierre != null) {
+                	        	cal.setTime(fechaCierre);
+                	        	String mesReclamo = mes[cal.get(Calendar.MONTH)];
+                	        	if (cal.get(Calendar.YEAR) == Year.now().getValue()) {
+                        			if (!htReclamosMensales.containsKey(mesReclamo)) {
+                        				htReclamosMensales.put(mesReclamo, 1);
+                        			} else {
+                        				htReclamosMensales.put(mesReclamo,  htReclamosMensales.get(mesReclamo) + 1);
+                        			}   	        		
+                	        	}
+            	        	}
+            	        }		
+            		}
+            		
+            		String[] headersReclamosMensuales = {"Mes", "Cantidad de Reclamos Tratados"};
+            	    DefaultTableModel tblReporteModelReclamosMensuales = new DefaultTableModel(headersReclamosMensuales, 0);
+            	    
+            		
+            		for(Entry<?, ?> entry: htReclamosMensales.entrySet()) {
+            			tblReporteModelReclamosMensuales.addRow(new Object[] {entry.getKey(), entry.getValue()});
+            		}
+            		
+            		tblReporte.setModel(tblReporteModelReclamosMensuales);
+            		
         			break;   
             	case 2: 
+            		//ranking de tratamiento de reclamos
+
         			System.out.println("2");
         			break;
             	case 3: 
+            		//tiempo promedio de respuesta de los reclamos por responsable
+            		
         			System.out.println("3");
         			break;
             	default:
