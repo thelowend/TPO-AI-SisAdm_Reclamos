d02745bc88d65fac36f6c03e1c7242b697b048fb
GleipnirYmir <ymirgleipnir@gmail.com>
2016-11-24 12:02:34 -0300

    Ordenando el código.

--- src/Model/ReclamoCantidades.java
+++ src/Model/ReclamoCantidades.java
@@ -3,17 +3,31 @@ package Model;
 import java.util.ArrayList;
 
 public class ReclamoCantidades extends Reclamo{
+
+	// ====================================
+	// == Attributes
+	// ====================================
+
+	private ArrayList<DetalleProducto> productos;    
+    
+	// ====================================
+	// == Constructor(s)
+	// ====================================
+
+	public ReclamoCantidades(){
+		productos = new ArrayList<DetalleProducto>();
+	}
+    
+	// ====================================
+	// == Accesors (Getters/Setters)
+	// ====================================    
+    
 	public ArrayList<DetalleProducto> getProductos() {
-		return productos;
+		return this.productos;
 	}
 
 	public void setProductos(ArrayList<DetalleProducto> productos) {
 		this.productos = productos;
 	}
-
-	private ArrayList<DetalleProducto> productos;
 	
-	public ReclamoCantidades(){
-		productos = new ArrayList<DetalleProducto>();
-	}
 }
