8427a49002c46f8de447cd881e16aa434b1f74ec
GleipnirYmir <ymirgleipnir@gmail.com>
2016-11-24 11:43:05 -0300

    Ordenando el código.

--- src/Model/DetalleProducto.java
+++ src/Model/DetalleProducto.java
@@ -1,26 +1,37 @@
 package Model;
 
 public class DetalleProducto {
+    
+	// ====================================
+	// == Attributes
+	// ====================================
+    
+    private Producto producto;
+    private int cantidad;    
+    
+	// ====================================
+	// == Constructor(s)
+	// ====================================    
+    
+    public DetalleProducto(){
+
+    }    
 
     public DetalleProducto(Producto producto, int cantidad) {
         this.producto = producto;
         this.cantidad = cantidad;
     }
+    
+	// ====================================
+	// == Accesors (Getters/Setters)
+	// ====================================    
 
     public Producto getProducto() {
-
-        return producto;
+        return this.producto;
     }
 
     public int getCantidad() {
-        return cantidad;
-    }
-
-    private Producto producto;
-    private int cantidad;
-
-    public DetalleProducto(){
-
+        return this.cantidad;
     }
 
 }
