900f33c77df4b62c9e9eb1cb7fef9ec49047fc97
GleipnirYmir <ymirgleipnir@gmail.com>
2016-11-24 12:07:26 -0300

    Ordenando el código.

--- src/Model/ReclamoProducto.java
+++ src/Model/ReclamoProducto.java
@@ -1,21 +1,31 @@
 package Model;
 
 public class ReclamoProducto extends Reclamo {
+    
+	// ====================================
+	// == Attributes
+	// ====================================    
+    
 	private DetalleProducto detalleProducto;
+    
+	// ====================================
+	// == Constructor(s)
+	// ====================================    
 
+	public ReclamoProducto(){
+
+	}
+    
+	// ====================================
+	// == Accesors (Getters/Setters)
+	// ====================================    
+    
 	public void setDetalleProducto(DetalleProducto detalleProducto) {
 		this.detalleProducto = detalleProducto;
 	}
 
 	public DetalleProducto getDetalleProducto() {
-
-		return detalleProducto;
-	}
-
-	public ReclamoProducto(){
-
+		return this.detalleProducto;
 	}
 	
 }
-	
-	
